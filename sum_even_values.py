1493a960548cd26683df9a12785a92b1a09f89ee
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-15 09:21:59 +0500

    14

--- sum_even_values.py
+++ sum_even_values.py
@@ -6,4 +6,17 @@ def sum_even_values(data: dict) -> int:
     Returns:
         int: The sum of all even values in the dictionary
     '''
-    return 
+    s=0
+    for i in data.values():
+        if i%2==0:
+            s+=i
+    return s
+data = {
+    1: 22, 
+    2: 3.5, 
+    4: 1, 
+    6: 7, 
+    5: 2, 
+    7: 3
+  }
+print(sum_even_values(data))
